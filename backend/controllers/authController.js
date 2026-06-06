7b3510bf3fbed2df5cf6951fed72d6a4119c85ea
GhostMan15 <farukuzicanin1@gmail.com>
2026-06-06 17:46:33 +0200

    added body of facelogin function

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,9 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const { error } = require("console");
 const jwt = require("jsonwebtoken");
 const path = require("path");
+const verifyFace = require("../controllers/verifyFace");
 
 exports.register = async (req, res) => {
     try {
@@ -20,10 +22,12 @@ exports.register = async (req, res) => {
         };
 
         if (req.file) {
-            const relativePath = path.relative(__dirname, req.file.path);
+            const relativePath = path.relative(path.join(__dirname, ".."),req.file.path);
             userData.faceImage = relativePath;
             console.log("File uploaded:", userData.faceImage);
         }
+    
+
 
         const user = await User.create(userData);
 
@@ -69,7 +73,48 @@ exports.login = async (req, res) => {
 };
 
 exports.faceLogin = async (req, res) => {
-    res.json({
-        message: "Face login placeholder"
-    });
+    try{
+        console.log("BODY:", req.body);
+        console.log("FILE:", req.file);
+        const {email, password}  = req.body;
+
+        if(!email || !password || !req.file){
+            return res.status(400).json({message: "Email, galso in sika so obvezni"});
+        }
+
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(400).json({message: "User ne obstaja"});
+        }
+       
+        const valid = await bcrypt.compare(password, user.passwordHash);
+
+        if (!valid) {
+            return res.status(400).json({ message: "Napacno geslo" });
+        } 
+        if (!user.faceImage) {
+            return res.status(400).json({ message: "Slika obraza ni registirarana" });
+        }
+
+        const registeredImagePath = path.join(__dirname, "..", user.faceImage);
+        console.log("Registered image path:", registeredImagePath);
+        const selfiePath = req.file.path;
+
+        const verifyResult = await verifyFace(registeredImagePath, selfiePath);
+
+        const token = jwt.sign(
+            { id: user._id, role: user.role },
+            process.env.JWT_SECRET,
+            { expiresIn: "7d" }
+        );
+
+        const safeUser = user.toObject();
+        delete safeUser.passwordHash;
+
+        res.json({ token, user: safeUser, faceVerification: verifyResult });
+    }catch(err){
+        res.status(500).json({error: err.message})
+    }
+   
 };
