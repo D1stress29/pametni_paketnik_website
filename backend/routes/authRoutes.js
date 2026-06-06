f7dfdbdf613cee97f22608298f9cab12c1142767
GhostMan15 <farukuzicanin1@gmail.com>
2026-06-06 17:46:15 +0200

    removed uneceseeri things

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,31 +1,11 @@
 const path = require("path");
 const fs = require("fs");
-const multer = require("multer");
+const upload = require("../config/multer");
 const router = require("express").Router();
 const controller = require("../controllers/authController");
 
-function safeFolderName(name) {
-  return name.trim().replace(/[^A-Za-z0-9._-]/g, "_") || "unknown_user";
-}
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const userFolder = safeFolderName(req.body.name);
-    const uploadDir = path.join(__dirname, "../uploads", userFolder);
-
-    fs.mkdirSync(uploadDir, { recursive: true });
-
-    cb(null, uploadDir);
-  },
-  filename: (req, file, cb) => {
-    cb(null, `face_${Date.now()}${path.extname(file.originalname) || ".jpg"}`);
-  }
-});
-
-const upload = multer({ storage });
-
 router.post("/register", upload.single("faceImage"), controller.register);
 router.post("/login", controller.login);
-router.post("/face-login", controller.faceLogin);
+router.post("/face-login", upload.single("faceImage"),controller.faceLogin);
 
 module.exports = router;
